80a6e871c20c84fdb4819f93c8daddb1b9d92228
d4g10ur0s <46010941+d4g10ur0s@users.noreply.github.com>
2023-01-11 14:10:48 +0200

    koble!
    exoume name kai awards,  prepei prin ta peraseis se domh na kaneis to name ari8mous

--- quotes_spider.py
+++ quotes_spider.py
@@ -2,8 +2,6 @@ import scrapy
 import os
 import json
 import re
-import pandas as pd
-import glob
 
 
 class QuotesSpider(scrapy.Spider):
@@ -17,7 +15,7 @@ class QuotesSpider(scrapy.Spider):
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
-    def write_file(self,scientist,name):
+    def write_file(self,scientist):
         path = os.getcwd()
         try :
             to_write = json.dumps(scientist)
@@ -25,7 +23,7 @@ class QuotesSpider(scrapy.Spider):
                 pass
             else:
                 os.mkdir(path + "\\scientists")
-            f = open(path + "\\scientists\\"+name+".json","w+")
+            f = open(path + "\\scientists\\"+scientist['name']+".json","w+")
             f.write(to_write)
             f.close()
         except :
@@ -34,54 +32,36 @@ class QuotesSpider(scrapy.Spider):
     async def parse_2(self,response):
         #metavlhth scientist
         scientist = {
-            'name' : "",
-            'awards' : 0,
+            'name' : '',
+            'uni' : [],
             'education_text' : None,
+            'awards' : 0,
         }
         #pairnw to viografiko ka8e scientist
         xp = '//*[@class=\"infobox biography vcard\"]'
         #prpei na parw ke text
         infobox = response.xpath(xp)
-        #
         #8elw onoma
-        #
         node = infobox.xpath('//*[@class=\"fn\"]')
         name = node.css('div::text').get()
-        name2 = None
-        try:
-            name2 = name.split(' ')
-            name2 = name2[len(name2)-1]
-            name3 =''.join(list(map(str,map(ord,name2))))
-            scientist['name'] = name3
-        except:
-            pass
-        #print(name)
+        scientist['name'] = name
         #input('ftasame sto name')
+        #pame gia awards
+        i=1
+        tr = infobox.xpath("//tr["+str(i)+"]")
+        while tr.get() != None :
+            if tr[0].css('th::text').get()!= None and "Awards" in tr[0].css('th::text').get():
+                ul = tr[0].css('td div ul')
+                if len(ul) == 0:#kapoia den einai se ul
+                    scientist['awards'] = len(tr[0].css('td').get().split("</a>"))-1
+                else:
+                    scientist['awards'] = len(ul.get().split("</li>"))-1
 
-        #
-        #gia na parw info
-        #
-        i = 1
-        while not(node.get()==None):
-            #//*[@id="mw-content-text"]/div[1]/table/tbody/tr[4]
-            xp = "//table/tbody/tr["+str(i)+"]"
-            node = infobox.xpath(xp)
-            #8elw alma mater
-            header = node.css('th::text').get()
-            if "wards" in str(header):
-                #pairnw to table data tou row me to awards
-                awards = node.css('td')
-                #pairnw to ta links gia ta awards...? kane ke su ena search mhn leipei kati...
-                for a in awards.css('a'):
-                    scientist['awards']+=1
-                    '''
-                    auto gia na kaneis debugging
-                    scientist["awards"].append[a.css('a::text').get()]
-                    '''
             i+=1
+            tr = infobox.xpath("//tr["+str(i)+"]")
         #end while
         #prepei na apo8hkeusw plhroforia
-        yield self.write_file(scientist,name2)
+        yield self.write_file(scientist)
 
     async def parse(self, response):
         #
@@ -92,22 +72,9 @@ class QuotesSpider(scrapy.Spider):
             #exw parei to lis
             lis = response.xpath(xp)
             #pairnw to link apo ka8e li
-            #input('prin mpei gia na parei apo ka8e li to link')
             for l in lis :
                 link = l.css('a::attr(href)').get()
                 if link is not None:
-                    #print(link)
-                    #print('*' * 30)
                     link = response.urljoin(link)
                     #paw se epomenh selida
                     yield scrapy.Request(link, callback=self.parse_2)
-                    #print('*' * 30)
-    
-    
-        # for scientist in scientists:
-        #     with open(scientist) as doc:
-        #         exp = json.load(doc)
-        #         scientists_list.append(exp)
-        # path_to_json = 'scientists/'
-        # json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
-        # print(json_files)  # for me this prints ['foo.json']
