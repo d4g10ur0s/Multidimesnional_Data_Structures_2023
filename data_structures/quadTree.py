698744fcc5befb6e676f5a238b6cb4fe023c62ff
d4g10ur0s <46010941+d4g10ur0s@users.noreply.github.com>
2023-01-31 13:12:39 +0200

    Add files via upload

--- data_structures/quadTree.py
+++ data_structures/quadTree.py
@@ -13,44 +13,44 @@ class QuadNode :
         self._empty = True
         self._info = []
         ''' intervals for parsing '''
-        #(intervals , child) || info
+        #child
         self.nw = None
         self.ne = None
         self.se = None
         self.sw = None
-
+        self.c = None
     def isLeaf(self):
         return self._leaf
     ''' go to next node '''
     def getDirections(self,info,toret=True):
-        if not(toret):
+        '''if not(toret):
             print(str(self.nw))
             print(str(self.ne))
             print(str(self.se))
             print(str(self.sw))
-            input('a')
+            input('a')'''
         arr=[]
-        tinfo = (info[0],info[1])
-        if (tinfo[0] >= self.nw[0][0]) and (tinfo[1] >= self.nw[1][0] ) :
+        tinfo = pd.DataFrame(data=info[:self._dim])
+        if (pd.DataFrame(data=tinfo).transpose()-self.c).mean().iloc[0] >= 0  :
             if toret:
-                return self.nw[2]
+                return self.nw
             else:
-                arr.append(self.nw[2])
-        elif (tinfo[0] >= self.ne[0][0] ) and (tinfo[1] <= self.ne[1][0] )  :
+                arr.append(self.nw)
+        elif (pd.DataFrame(data=tinfo).transpose()-self.c).mean().iloc[0] <= 0   :
             if toret:
-                return self.ne[2]
+                return self.ne
             else:
-                arr.append(self.ne[2])
-        elif (tinfo[0] <= self.se[0][0] ) and (tinfo[1] <= self.se[1][0] )  :
+                arr.append(self.ne)
+        elif (pd.DataFrame(data=tinfo).transpose()+self.c).mean().iloc[0] <= 0   :
             if toret:
-                return self.se[2]
+                return self.se
             else:
-                arr.append(self.se[2])
-        elif (tinfo[0] <= self.sw[0][0] ) and (tinfo[1] >= self.sw[1][0])  :
+                arr.append(self.se)
+        elif (pd.DataFrame(data=tinfo).transpose()+self.c).mean().iloc[0] >= 0   :
             if toret:
-                return self.sw[2]
+                return self.sw
             else:
-                arr.append(self.sw[2])
+                arr.append(self.sw)
         if toret:
             return None
         else:
@@ -67,54 +67,72 @@ class QuadNode :
         for i in self._info:
             arr.append(i[coord])
         return arr
+    '''def toNW(self,vector):
+        for i in range(self._dim):
+            if vector[i]-self.c[i]<0:
+                return False
+        return True
+    def toNE(self,vector):
+        for i in range(self._dim):
+            if vector[i]-self.c[i]>0:
+                return False
+        return True
+    def toNW(self,vector):
+        for i in range(self._dim):
+            if vector[i]-self.c[i]<0:
+                return False
+        return True
+    def toNW(self,vector):
+        for i in range(self._dim):
+            if vector[i]-self.c[i]<0:
+                return False
+        return True'''
     def getSplit(self,parent):
         self._leaf = False
         ''' info is an array of (x,y,information) elements '''
         #1. se center to b the mean of x and y
-        x = pd.DataFrame(data=self.get1Coord(0)).mean().iloc[0]
-        y = pd.DataFrame(data=self.get1Coord(1)).mean().iloc[0]
+        temp = []
+        for i in self._info:
+            temp.append(i[:len(i)-1])#den pairnw to teleutaio
+        self.c = pd.DataFrame(data=temp).transpose().mean()
         #2. create nodes
         #a. NW node
-        self.nw = [(x-x*1e-2,x*2+0.5), (y+y*1e-2,y*2+0.5)]
         nw = QuadNode(self._dim,self._max,parent=parent,leaf=True)
         nw.clearNode()
         i=0
         while i < len(self._info) :
             t = self._info[i]
-            if (t[0] >= self.nw[0][0] ) and (t[1] >= self.nw[1][0]) :
+            if (pd.DataFrame(data=t).transpose()-self.c).mean().iloc[0] > 0 :
                 nw.insertInfo(t)
                 self._info.pop(i)
             i+=1
         #b. NE node
-        self.ne = [(x-x*1e-2,x*2+0.5), (y-y*1e-2,y*(-2)-0.5)]
         ne = QuadNode(self._dim,self._max,parent=parent,leaf=True)
         ne.clearNode()
         i=0
         while i < len(self._info) :
             t = self._info[i]
-            if (t[0] >= self.ne[0][0] ) and (t[1] <= self.ne[1][0]) :
+            if (pd.DataFrame(data=t).transpose()-self.c).mean().iloc[0] <= 0 :
                 ne.insertInfo(t)
                 self._info.pop(i)
             i+=1
         #c. SE node
-        self.se = [(x+x*1e-2,x*(-2)-0.5), (y-y*1e-2,y*(-2)-0.5)]
         se = QuadNode(self._dim,self._max,parent=parent,leaf=True)
         se.clearNode()
         i=0
         while i < len(self._info) :
             t = self._info[i]
-            if (t[0] <= self.se[0][0] ) and (t[1] <= self.se[1][0]) :
+            if (pd.DataFrame(data=t).transpose()+self.c).mean().iloc[0] < 0:
                 se.insertInfo(t)
                 self._info.pop(i)
             i+=1
         #d. SW node
-        self.sw = [(x+x*1e-2,x*(-2)-0.5), (y+y*1e-2,y*2+0.5)]
         sw = QuadNode(self._dim,self._max,parent=parent,leaf=True)
         sw.clearNode()
         i=0
         while i < len(self._info) :
             t = self._info[i]
-            if (t[0] <= self.sw[0][0] ) and (t[1] >= self.sw[1][0]) :
+            if (pd.DataFrame(data=t).transpose()+self.c).mean().iloc[0] >= 0 :
                 sw.insertInfo(t)
                 self._info.pop(i)
             i+=1
@@ -123,30 +141,45 @@ class QuadNode :
         self._leaf=False
         temp = self._info
         self._info=None
-        self.nw.append(kids)
-        self.ne.append(kids+1)
-        self.se.append(kids+2)
-        self.sw.append(kids+3)
+        self.nw=(kids)
+        self.ne=(kids+1)
+        self.se=(kids+2)
+        self.sw=(kids+3)
         return temp
     def clearNode(self):
         self._info = []
     def getCoordinates(self):
-        return [self.nw[2],self.ne[2],self.se[2],self.sw[2]]
+        return [self.nw,self.ne,self.se,self.sw]
     def printNode(self):
         if self._leaf:
             for i in self._info:
-                print(str(i[2]))
-        else:
+                print(str(i[-1]))
+        '''else:
             print(str(self.nw))
             print(str(self.ne))
             print(str(self.se))
-            print(str(self.sw))
+            print(str(self.sw))'''
     def getInfo(self):
         arr = []
         for i in self._info:
-            arr+=i[2]
+            arr+=i[-1]
         return arr
+    def adjustPointer(self,indx):
+        #1. parent
+        if self._parent==None:
+            pass
+        elif self._parent>indx:
+            self._parent-=1
 
+        if not self._leaf:
+            if self.nw > indx:
+                self.nw-=1
+            if self.ne > indx:
+                self.ne-=1
+            if self.se > indx:
+                self.se-=1
+            if self.sw > indx:
+                self.sw-=1
 class QuadTree :
     _nodes = None
     def __init__(self,dim,max,info=None):
@@ -156,21 +189,22 @@ class QuadTree :
             k=0
             for i in info :
                 print(10*"*"+" " +str(k)+" "+"*"*10)
-                self.insert(self.fromDim2D(i))
+                self.insert(i)
                 k+=1
-    ''' to transform from dim to 2d '''
+    ''' to transform from dim to 2d
     def fromDim2D(self,info,ins=True):
         x=0
         for i in range(self._dim-1):
             x+=info[i]
         if ins:
-            return (x/(self._dim-1),info[self._dim-1],info[self._dim])
+            return (x/(self._dim-1),info[-2],info[-1])
         else:
-            print(str(info))
             try :
                 return (x/(self._dim-1),info[self._dim-1])#epeidh den pernw ta awards
             except :
                 return (x/(self._dim-1),0)#epeidh den pernw ta awards
+    Vghke Akuro!
+    '''
     def printQTree(self,i=0):
         print("Node : " + str(i))
         #input(str(self._nodes[i].printNode()))
@@ -216,13 +250,13 @@ class QuadTree :
         return lindx
 
     def findLeaves(self,info,indx=0):
-        input(str(info))
+        #input(str(info))
         arr = []
         if self._nodes[indx].isLeaf():
             return indx
         else:
             kids = self._nodes[indx].getDirections(info,toret=False)
-            input(str(kids))
+            #input(str(kids))
             if not(kids==None):
                 for k in kids:
                     self._nodes[k].printNode()
@@ -234,11 +268,42 @@ class QuadTree :
         return arr
 
     def qSearch(self,info):
-        search_for = self.fromDim2D(info,ins=False)
-        indx = self.findLeaves(search_for)
+        indx = self.findLeaves(info)
         arr = []
         for i in indx:
             arr+=self._nodes[i].getInfo()
-
-        print(str(i))
+        for i in range(len(arr)) :
+            print("*"*10+" " + str(i)+ " " + "*"*10)
+            print(str(arr[i]))
         return arr
+
+    def qDelete(self,indx,entry):
+        self._nodes[indx].rmEntry(entry)
+        while self._nodes[indx].isEmpty():
+            for i in self._nodes[self._nodes[indx].getParent()].getChildren():
+                if not self._nodes[i].isEmpty():
+                    return False
+            #prepei na svhsw nodes ke na kanw merge me parent
+            a,b,c,d = self._nodes[self._nodes[indx].getParent()].getChildren()
+            self._nodes.pop(a)
+            if a > b :
+                b-=1
+            if a > c:
+                c-=1
+            if a>d:
+                d-=1
+            self._nodes.pop(b)
+            if b>c:
+                c-=1
+            if b>d:
+                d-=1
+            self._nodes.pop(c)
+            if c>d:
+                d-=1
+            self._nodes.pop(d)
+
+            for i in self._nodes[self._nodes[indx].getParent()].getChildren():
+                for k in self._nodes:
+                    k.adjustPointer(i)
+            #lets check parent
+            indx = self._nodes[indx].getParent()
