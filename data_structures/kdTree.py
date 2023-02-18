5423934a9b1665d6b120ca0b66ed9694ec3abd32
d4g10ur0s <46010941+d4g10ur0s@users.noreply.github.com>
2023-02-18 11:32:49 +0200

    Add files via upload

--- data_structures/kdTree.py
+++ data_structures/kdTree.py
@@ -4,30 +4,29 @@ import pandas as pd
 class KDTree:
     def __init__(self, points):
         self.root = self.build(points)
-        
-        
+
     def build(self, points, dim=0):
         if not points:
             return None
-        
+
         # Sort the points by the current dimension
         points.sort(key=lambda point: point[dim])
-        
+
         # Get the median point
         mid = len(points) // 2
         median = points[mid][:-1]
-        
+
         # Recursively build the left and right subtrees
         left_subtree = self.build(points[:mid], (dim + 1) % len(points[0]))
         right_subtree = self.build(points[mid + 1:], (dim + 1) % len(points[0]))
-        
+
         # Return the current node
-        return Node(median, left_subtree, right_subtree,points[mid][-1])
+        return Node(median, left_subtree, right_subtree,points[mid][-2],points[mid][-1])
 
     def delete(self, root, point):
         if not root:
             return None
-        print('point',point)
+
         if point[0] < root.point[0]:
             root.left = self.delete(root.left, point)
         elif point[0] > root.point[0]:
@@ -53,12 +52,12 @@ class KDTree:
 
 
 class Node:
-    def __init__(self, point, left=None, right=None,name=''):
+    def __init__(self, point, left=None, right=None,name='',awards=0):
         self.point = point
         self.left = left
         self.right = right
         self.name = name
-    
+
     def kd_search(self,name,dim=0):
         if dim+1==self.dim:
             dim = 0
@@ -69,15 +68,14 @@ class Node:
             return self.point
         if self.point[dim] < name[dim]:
             a=self.left.kd_search(name,dim)
-            arr.append(a)  
+            arr.append(a)
         else:
             a=self.right.kd_search(name,dim)
             arr.append(a)
         return arr
-        
+
 def printNode(node, string=""):
     if node is not None:
-        print(string + "|Name:" + str(node.name))
+        print(string + "|Name:" + str(node.name) + "|Awards:" + str(node.awards))
         printNode(node.left, "\t" + string + "-left-")
         printNode(node.right, "\t" + string + "-right-")
-
